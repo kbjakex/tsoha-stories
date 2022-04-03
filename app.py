d6f58944154b61d7f5d38d7e2f43cbcb33b3c790
kbjakex <90567676+kbjakex@users.noreply.github.com>
2022-04-03 17:03:22 +0300

    Start work

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask import redirect, render_template, request, session
+from werkzeug.security import check_password_hash, generate_password_hash
+from flask_sqlalchemy import SQLAlchemy
+from os import getenv
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+app.secret_key = getenv("SECRET_KEY")
+db = SQLAlchemy(app)
+
+@app.route("/")
+def index():
+    db.session.execute("INSERT INTO visitors (time) VALUES (NOW())")
+    db.session.commit()
+    result = db.session.execute("SELECT COUNT(*) FROM visitors")
+    counter = result.fetchone()[0]
+    return render_template("index.html", counter=counter) 
